054b38f3b76a7020e7b5fb140d095a8745f7b941
Scott Wang <scott@malloc.sh>
2017-09-10 14:57:56 -0700

    decrpt

--- bin/put.php
+++ bin/put.php
@@ -11,95 +11,102 @@ function read_stdin()
     return $input;                  // return the text entered
 }
 
+try {
+    $options = getopt('', ['key::', 'override::', 'region::', 'alias::', 'dynamo::']);
+    if (empty($options['key'])) {
+        throw new Exception("Key is required. php ./vendor/put.php --key=[your_key]");
+    }
 
-$options = getopt('', ['key::', 'override::', 'region::', 'alias::', 'dynamo::']);
-$key = preg_replace('~[^\\pL\d]+~u', '-', trim($options['key']));
+    $key = preg_replace('~[^\\pL\d]+~u', '-', trim($options['key']));
 
-if (empty($key)) {
-    throw new Exception("Key is required. php ./vendor/put.php --key=[your_key]");
-}
+    if (empty($key)) {
+        throw new Exception("Key is required. php ./vendor/put.php --key=[your_key]");
+    }
 
-$override = empty($options['override']) ? false : filter_var($options['override'], FILTER_VALIDATE_BOOLEAN);
-$region = empty($options['region']) ? "us-west-2" : $options['region'];
-$alias = empty($options['alias']) ? "rw-secret" : $options['alias'];
-$dynamoTable = empty($options['dynamo']) ? "secrets" : $options['dynamo'];
+    $override = empty($options['override']) ? false : filter_var($options['override'], FILTER_VALIDATE_BOOLEAN);
+    $region = empty($options['region']) ? "us-west-2" : $options['region'];
+    $alias = empty($options['alias']) ? "rw-secret" : $options['alias'];
+    $dynamoTable = empty($options['dynamo']) ? "secrets" : $options['dynamo'];
 
-$marshaller = new \Aws\DynamoDb\Marshaler();
-$dynamoDBClient = new \Aws\DynamoDb\DynamoDbClient([
-    "region" => $region,
-    "version" => "2012-08-10"
-]);
-
-/** @var $itemResult \Aws\Result */
-$itemResult = $dynamoDBClient->getItem([
-    'TableName' => $dynamoTable,
-    'Key' => array(
-        'id' => array('S' => $key)
-    ),
-    'ConsistentRead' => true,
-]);
-
-if (!empty($itemResult->get("Item")) && $override === false) {
-    throw new Exception("Key already exists. Override the secret by --override.");
-}
-
-$kmsConfig = [
-    "region" => $region,
-    "version" => "2014-11-01"
-];
-$kmsClient = new \Aws\Kms\KmsClient($kmsConfig);
-
-//generate new secret automatically
-if ($override === false) {
-    $kmsResult = $kmsClient->generateDataKey([
-        "KeyId" => "alias/$alias",
-        "KeySpec" => "AES_256"
+    $marshaller = new \Aws\DynamoDb\Marshaler();
+    $dynamoDBClient = new \Aws\DynamoDb\DynamoDbClient([
+        "region" => $region,
+        "version" => "2012-08-10"
     ]);
 
-    $encodedSecret = base64_encode($kmsResult->get("CiphertextBlob"));
-} else {
-    echo "Please input the secret ";
-    $secret = read_stdin();
-    if (strlen($secret) < 8) {
-        throw new \InvalidArgumentException("Secret too short!");
-    }
-
-    if (!preg_match("#[0-9]+#", $secret)) {
-        throw new \InvalidArgumentException("Secret must include at least one number!");
-    }
-
-    if (!preg_match("#[a-zA-Z]+#", $secret)) {
-        throw new \InvalidArgumentException("Secret must include at least one letter!");
-    }
-
-    //encrypt the secret
-    $kmsResult = $kmsClient->encrypt([
-        "KeyId" => "alias/$alias",
-        "Plaintext" => $secret,
-    ]);
-    $encodedSecret = base64_encode($kmsResult->get("CiphertextBlob"));
-}
-
-if (empty($itemResult->get("Item"))) {
-    $dynamoDBClient->putItem(array(
-        'TableName' => $dynamoTable,
-        'Item' => $marshaller->marshalItem(["id" => $key, "secret" => $encodedSecret]),
-        'ConditionExpression' => 'attribute_not_exists(id)',
-        'ReturnValues' => 'ALL_OLD'
-    ));
-} else {
-    $updateAttributes = [
+    /** @var $itemResult \Aws\Result */
+    $itemResult = $dynamoDBClient->getItem([
         'TableName' => $dynamoTable,
         'Key' => array(
-            'id' => $marshaller->marshalValue($key)
+            'id' => array('S' => $key)
         ),
-        'ExpressionAttributeNames' => ["#secret" => "secret"],
-        'ExpressionAttributeValues' =>  [":secret" => $marshaller->marshalValue($encodedSecret)],
-        'ConditionExpression' => 'attribute_exists(id)',
-        'UpdateExpression' => "set #secret = :secret",
-        'ReturnValues' => 'ALL_NEW'
-    ];
+        'ConsistentRead' => true,
+    ]);
 
-    $dynamoDBClient->updateItem($updateAttributes);
+    if (!empty($itemResult->get("Item")) && $override === false) {
+        throw new Exception("Key already exists. Override the secret by --override.");
+    }
+
+    $kmsConfig = [
+        "region" => $region,
+        "version" => "2014-11-01"
+    ];
+    $kmsClient = new \Aws\Kms\KmsClient($kmsConfig);
+
+//generate new secret automatically
+    if ($override === false) {
+        $kmsResult = $kmsClient->generateDataKey([
+            "KeyId" => "alias/$alias",
+            "KeySpec" => "AES_256"
+        ]);
+
+        $encodedSecret = base64_encode($kmsResult->get("CiphertextBlob"));
+    } else {
+        echo "Please input the secret ";
+        $secret = read_stdin();
+        if (strlen($secret) < 8) {
+            throw new \InvalidArgumentException("Secret too short!");
+        }
+
+        if (!preg_match("#[0-9]+#", $secret)) {
+            throw new \InvalidArgumentException("Secret must include at least one number!");
+        }
+
+        if (!preg_match("#[a-zA-Z]+#", $secret)) {
+            throw new \InvalidArgumentException("Secret must include at least one letter!");
+        }
+
+        //encrypt the secret
+        $kmsResult = $kmsClient->encrypt([
+            "KeyId" => "alias/$alias",
+            "Plaintext" => $secret,
+        ]);
+        $encodedSecret = base64_encode($kmsResult->get("CiphertextBlob"));
+    }
+
+    if (empty($itemResult->get("Item"))) {
+        $dynamoDBClient->putItem(array(
+            'TableName' => $dynamoTable,
+            'Item' => $marshaller->marshalItem(["id" => $key, "secret" => $encodedSecret]),
+            'ConditionExpression' => 'attribute_not_exists(id)',
+            'ReturnValues' => 'ALL_OLD'
+        ));
+    } else {
+        $updateAttributes = [
+            'TableName' => $dynamoTable,
+            'Key' => array(
+                'id' => $marshaller->marshalValue($key)
+            ),
+            'ExpressionAttributeNames' => ["#secret" => "secret"],
+            'ExpressionAttributeValues' => [":secret" => $marshaller->marshalValue($encodedSecret)],
+            'ConditionExpression' => 'attribute_exists(id)',
+            'UpdateExpression' => "set #secret = :secret",
+            'ReturnValues' => 'ALL_NEW'
+        ];
+
+        $dynamoDBClient->updateItem($updateAttributes);
+    }
+    echo "ok\n";
+} catch (Exception $e) {
+    echo "failed. [Exception: {$e->getMessage()}]\n";
 }
-echo "ok\n";
--- src/Secret.php
+++ src/Secret.php
@@ -1,7 +1,56 @@
 <?php
 namespace RW;
 
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Marshaler;
+use Aws\Kms\KmsClient;
+
 class Secret
 {
+    /**
+     * Get the secret
+     *
+     * @param $key
+     * @param string $region
+     * @param string $table
+     *
+     * @return mixed|null
+     * @throws \Exception
+     */
+    public static function get($key, $region = "us-west-2", $table = "secrets")
+    {
+        $dynamoDBClient = new DynamoDbClient([
+            "region" => $region,
+            "version" => "2012-08-10"
+        ]);
+
+        /** @var $itemResult \Aws\Result */
+        $itemResult = $dynamoDBClient->getItem([
+            'TableName' => $table,
+            'Key' => array(
+                'id' => array('S' => $key)
+            ),
+            'ConsistentRead' => true,
+        ]);
+
+        if (empty($itemResult->get("Item"))) {
+            throw new \Exception("Key does not exists.");
+        }
+
+        $marshaller = new Marshaler();
+        $dynamoResult = $marshaller->unmarshalItem($itemResult->get('Item'));
+        $encodedSecret = $dynamoResult['secret'];
+
+        $kmsConfig = [
+            "region" => $region,
+            "version" => "2014-11-01"
+        ];
+        $kmsClient = new KmsClient($kmsConfig);
+        $kmsResult = $kmsClient->decrypt([
+            'CiphertextBlob' => base64_decode($encodedSecret),
+        ]);
+
+        return $kmsResult->get('Plaintext');
+    }
 
 }
